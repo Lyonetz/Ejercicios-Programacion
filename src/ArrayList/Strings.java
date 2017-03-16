3c03700eada9353a2a1c80a2ad571290128cc9e5
Lyonetz <carlosjavierleon322@gmail.com>
2017-03-16 13:12:21 +0100

    refreshing...

--- src/ArrayList/Strings.java
+++ src/ArrayList/Strings.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Strings {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
