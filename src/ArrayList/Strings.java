50cb984f5156ed335418639c67e638a8e35b4c29
Lyonetz <carlosjavierleon322@gmail.com>
2017-03-16 12:48:37 +0100

    refreshing...

---
+++ src/ArrayList/Strings.java
@@ -0,0 +1,29 @@
+package ArrayList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Strings {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> arrayList = new ArrayList<>();
+        String s;
+        boolean check = false;
+
+        do {
+            System.out.println("Introduzca un nuevo String (asdf para parar): ");
+            s = br.readLine();
+            if(s.equalsIgnoreCase("asdf")) {
+                check = true;
+            } else {
+                arrayList.add(s);
+            }
+        }while(!check);
+
+        for(String e : arrayList){
+            System.out.println(e);
+        }
+    }
+}
