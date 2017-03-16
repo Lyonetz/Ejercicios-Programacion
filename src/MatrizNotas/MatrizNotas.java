545fad7061ede2041317ded7242d8e1cdcacfc7c
Lyonetz <carlosjavierleon322@gmail.com>
2017-03-16 12:31:36 +0100

    refreshing...

--- src/MatrizNotas/MatrizNotas.java
+++ src/MatrizNotas/MatrizNotas.java
@@ -18,24 +18,19 @@ public class MatrizNotas {
         double matrizNotas[][] = new double[6][4];
 
         Random rnd = new Random();
-        for (int i = 0; i < matrizNotas.length; i++) {
-            for (int j = 0; j < matrizNotas[i].length; j++) {
+        for (int i = 0; i < matrizNotas.length; i++)
+            for (int j = 0; j < matrizNotas[i].length; j++)
                 matrizNotas[i][j] = rnd.nextInt(11);
-            }
-        }
 
         mostrarMatriz(matrizNotas);
 
         double matrizPonderaciones[] = {0.2,0.4,0.2,0.2};
         double notasFinales[][] = new double[6][1];
 
-        for (int i = 0; i < notasFinales.length; i++) {
-            for (int j = 0; j < notasFinales[i].length; j++) {
-                for (int k = 0; k < matrizNotas[0].length; k++) {
+        for (int i = 0; i < notasFinales.length; i++)
+            for (int j = 0; j < notasFinales[i].length; j++)
+                for (int k = 0; k < matrizNotas[0].length; k++)
                     notasFinales[i][j] += matrizNotas[i][k] * matrizPonderaciones[k];
-                }
-            }
-        }
         mostrarMatriz(notasFinales);
     }
 }
