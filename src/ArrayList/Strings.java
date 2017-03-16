960ddb211e8e9377ccaf87a5511e0389d39908b0
Lyonetz <carlosjavierleon322@gmail.com>
2017-03-16 13:04:34 +0100

    refreshing...

--- src/ArrayList/Strings.java
+++ src/ArrayList/Strings.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Strings {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +21,8 @@ public class Strings {
             else
                 arrayList.add(s);
         }while(!check);
+
+        Collections.sort(arrayList);
 
         for(String e : arrayList)
             System.out.println("- " + e);
