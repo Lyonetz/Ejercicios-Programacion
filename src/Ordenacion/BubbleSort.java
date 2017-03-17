14f18a28581e3bf4db91ca1e154deb73732a2b71
Lyonetz <carlosjavierleon322@gmail.com>
2017-03-17 09:43:40 +0100

    refreshing...

---
+++ src/Ordenacion/BubbleSort.java
@@ -0,0 +1,28 @@
+package Ordenacion;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int A[]) {
+        int aux;
+
+        for (int i = A.length; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                if(A[j] > A[j + 1]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int vect[] = {2,9,7,1};
+
+        vect = bubbleSort(vect);
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+    }
+}
