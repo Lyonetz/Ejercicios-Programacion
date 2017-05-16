d0cddcd7c1d009eb43961c2c95a4ad82d9084863
Lyonetz <carlosjavierleon322@gmail.com>
2017-05-16 12:09:56 +0200

    @Carlos

--- src/GUI/ConvertidorEurosDolares.java
+++ src/GUI/ConvertidorEurosDolares.java
@@ -52,11 +52,12 @@ public class ConvertidorEurosDolares extends JPanel {
         //======== this ========
 
         // JFormDesigner evaluation mark
-        setBorder(new javax.swing.border.CompoundBorder(
+        ConvertidorChecker.setBorder(new javax.swing.border.CompoundBorder(
             new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
                 "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
                 javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+                java.awt.Color.red), ConvertidorChecker.getBorder()));
+        ConvertidorChecker.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
         setLayout(new MigLayout(
             "hidemode 3",
